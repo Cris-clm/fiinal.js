b98db890d2f88de2a2545150c6de76dd94793a1b
Cristian <cristianleo_5@hotmail.com>
2023-01-19 02:37:14 -0300

    con fetch funcionanado

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 
-let articulos =
+/*let articulos =
     [
         { id: 10, prenda: "zapatos", nombre: "zapatos Enzo Bonafé A", categoria: "casual", precio: 14150, marca: "Enzo Bonafé", color: "marron", imagen: "http://drive.google.com/uc?export=view&id=1nomq9AHEnzqzUQpRcUlDZyLcbmDZ55O9" },
         { id: 11, prenda: "zapatos", nombre: "zapatos Enzo Bonafé B", categoria: "casual", precio: 19000, marca: "Enzo Bonafé", color: "negro", imagen: "http://drive.google.com/uc?export=view&id=1rfTS9VS7Ee0Yze-2CdmL7ZBjfj0DsXu8" },
@@ -31,79 +31,88 @@ let articulos =
         { id: 52, prenda: "camisas", nombre: "Camisa Hombre Entallada C", categoria: "vestir", precio: 25100, marca: "Dior", color: "Lisa Gris", imagen: "http://drive.google.com/uc?export=view&id=19Ai4kM0WTviMO8iE7GJ3NMxDD_FycU8f" },
         { id: 53, prenda: "camisas", nombre: "Camisa Hombre Entallada D", categoria: "vestir", precio: 22000, marca: "Dior", color: "Lisa Rosa", imagen: "http://drive.google.com/uc?export=view&id=1cB0zNOt2FAXKF9Nn5b2OR3AB707Uw6yW" },
         { id: 54, prenda: "camisas", nombre: "Camisa Hombre Entallada E", categoria: "vestir", precio: 22300, marca: "Dior", color: "Lisa Salmon", imagen: "http://drive.google.com/uc?export=view&id=1ILE62G9XxMc_rmqTomLwf0wwuAOB28oO" },
-        { id: 55, prenda: "camisas", nombre: "camisa hombre Oxford F", categoria: "vestir", precio: 22100, marca: "Bowen", color: "gris claro", imagen: "http://drive.google.com/uc?export=view&id=1sNArYMM7EmoHG317VIvgvfGzTtKtMW6-" },
-        { id: 56, prenda: "camisas", nombre: "camisa hombre Oxford G", categoria: "vestir", precio: 22250, marca: "Bowen", color: "gris topo", imagen: "http://drive.google.com/uc?export=view&id=1BOC6zi2Wtv4EC23-SjeiDoDD2eBu4MZq" },
-    ]
+        { id: 55, prenda: "camisas", nombre: "camisa hombre Oxford F", categoria: "vestir", precio: 22100, marca: "Bowen", color: "gris claro", imagen: "http://drive.google.com/uc?export=view&id=1sNArYMM7EmoHG317VIvgvfGzTtKtMW6-" },         { id: 56, prenda: "camisas", nombre: "camisa hombre Oxford G", categoria: "vestir", precio: 22250, marca: "Bowen", color: "gris topo", imagen: "http://drive.google.com/uc?export=view&id=1BOC6zi2Wtv4EC23-SjeiDoDD2eBu4MZq" },
+   ]*/
+
+fetch("../json/articulos.json")
+    .then(response => response.json())
+    .then(articulos => programaTotal(articulos));
 
 
-let contenedorRenderCarrito = document.getElementById("contenedorRenderCarrito")
-let contenedoRender = document.getElementById("contenedoRender")
+    // Funcion geenral contiene todo el programa
+    function programaTotal(articulos){
+    
+    let contenedorRenderCarrito = document.getElementById("contenedorRenderCarrito")
+        let contenedoRender = document.getElementById("contenedoRender")
 
-let carrito = []
-if (localStorage.getItem("carrito")) {
-    carrito = JSON.parse(localStorage.getItem("carrito"))
-}
-renderCarrito(carrito)
+        let carrito = []
+        if (localStorage.getItem("carrito")) {
+            carrito = JSON.parse(localStorage.getItem("carrito"))
+        }
+        renderCarrito(carrito)
 
-//declacion de variables 
-let filtroZapatos = document.getElementById("zapatos")
-let filtroPantalones = document.getElementById("pantalones")
-let filtroBuzos = document.getElementById("buzos")
-let filtroCardigan = document.getElementById("cardigan")
-let filtroCamisas = document.getElementById("camisas")
-let cantidadCarrito = document.getElementById("cantidadCarrito")
-let inicio = document.getElementById("inicio")
+        //declacion de variables 
+        let filtroZapatos = document.getElementById("zapatos")
+        let filtroPantalones = document.getElementById("pantalones")
+        let filtroBuzos = document.getElementById("buzos")
+        let filtroCardigan = document.getElementById("cardigan")
+        let filtroCamisas = document.getElementById("camisas")
+        let cantidadCarrito = document.getElementById("cantidadCarrito")
+        let inicio = document.getElementById("inicio")
 
+        let btnComprar = document.getElementById("comprar")
+        btnComprar.addEventListener("click", () => {
+            Swal.fire({
+                title: 'Gracias por su Compra',
+                text: 'Disfrute su Compra',
+                imageUrl: '../images/logito.png',
+                icon: 'success',
+                confirmButtonText: 'Cerrar',
+                imageAlt: 'imagen logo',
+            })
 
-
-let btnComprar = document.getElementById("comprar")
-btnComprar.addEventListener("click", () => {
-    localStorage.removeItem("carrito")
-    carrito = []
-    renderCarrito(carrito)
-})
-
-filtroZapatos.addEventListener("click", renderizarArticulosFiltrados)
-filtroZapatos.addEventListener("click", ocultarSlider)
-filtroPantalones.addEventListener("click", renderizarArticulosFiltrados)
-filtroPantalones.addEventListener("click", ocultarSlider)
-filtroBuzos.addEventListener("click", renderizarArticulosFiltrados)
-filtroBuzos.addEventListener("click", ocultarSlider)
-filtroCardigan.addEventListener("click", renderizarArticulosFiltrados)
-filtroCardigan.addEventListener("click", ocultarSlider)
-filtroCamisas.addEventListener("click", renderizarArticulosFiltrados)
-filtroCamisas.addEventListener("click", ocultarSlider)
-inicio.addEventListener("click", volverInicio)
-
-
-
-
-//funcion de filtrado de articulos 
-
-function renderizarArticulosFiltrados(e) {
-    if (e.target.id == "todos") {
-        renderizarContenedor(articulos)
-    } else {
-        renderizarContenedor(articulos.filter(articulo => articulo.prenda.toLowerCase() == e.target.id))
-    }
-}
-    comprobar(carrito)
-
-    function comprobar() {
-        if (localStorage.getItem("Carrito")) {
-            carrito = JSON.parse(localStorage.getItem("Carrito"))
+            localStorage.removeItem("Carrito")
+            carrito = []
             renderCarrito(carrito)
-           
-        } 
-    }
+        })
 
-//funcion de filtrado de renderezar articulos 
-function renderizarContenedor(arrayDeProductos) {
-    contenedoRender.innerHTML = ""
-    for (const producto of arrayDeProductos) {
-        let tarjeta = document.createElement("div")
-        tarjeta.className = "tarjeta"
-        tarjeta.innerHTML = `
+        filtroZapatos.addEventListener("click", renderizarArticulosFiltrados)
+        filtroZapatos.addEventListener("click", ocultarSlider)
+        filtroPantalones.addEventListener("click", renderizarArticulosFiltrados)
+        filtroPantalones.addEventListener("click", ocultarSlider)
+        filtroBuzos.addEventListener("click", renderizarArticulosFiltrados)
+        filtroBuzos.addEventListener("click", ocultarSlider)
+        filtroCardigan.addEventListener("click", renderizarArticulosFiltrados)
+        filtroCardigan.addEventListener("click", ocultarSlider)
+        filtroCamisas.addEventListener("click", renderizarArticulosFiltrados)
+        filtroCamisas.addEventListener("click", ocultarSlider)
+        inicio.addEventListener("click", volverInicio)
+
+        //funcion de filtrado de articulos 
+
+        function renderizarArticulosFiltrados(e) {
+            if (e.target.id == "todos") {
+                renderizarContenedor(articulos)
+            } else {
+                renderizarContenedor(articulos.filter(articulo => articulo.prenda.toLowerCase() == e.target.id))
+            }
+        }
+        comprobacionCarrito(carrito)
+
+        function comprobacionCarrito() {
+            if (localStorage.getItem("Carrito")) {
+                carrito = JSON.parse(localStorage.getItem("Carrito"))
+                renderCarrito(carrito)
+            }
+        }
+
+        //funcion de filtrado de renderezar articulos 
+        function renderizarContenedor(arrayDeProductos) {
+            contenedoRender.innerHTML = ""
+            for (const producto of arrayDeProductos) {
+                let tarjeta = document.createElement("div")
+                tarjeta.className = "tarjeta"
+                tarjeta.innerHTML = `
         <h3>${producto.nombre}</h3>
         <h4>$${producto.precio}</h4>
         <h4>${producto.marca}</h4>
@@ -111,84 +120,105 @@ function renderizarContenedor(arrayDeProductos) {
         <img src=${producto.imagen}>
         <button class="btn" id= ${producto.id}>Agregar al Carrito</button>
         `
-        contenedoRender.appendChild(tarjeta)
-    }
-    let botones = document.getElementsByClassName("btn")
-    for (const btn of botones) {
-        btn.addEventListener("click", agreCarrito)
-    }
-}
+                contenedoRender.appendChild(tarjeta)
+            }
+            let botones = document.getElementsByClassName("btn")
+            for (const btn of botones) {
+                btn.addEventListener("click", agreCarrito)
+            }
+        }
 
 
-//funcion de agregar a carrito
-function agreCarrito(e) {
-    let articuloBuscado = articulos.find(articulo => articulo.id == e.target.id)
-    let posicionArticuloBuscado = carrito.findIndex(articulo => articulo.id == articuloBuscado.id)
+        //funcion de agregar a carrito
+        function agreCarrito(e) {
+            let articuloBuscado = articulos.find(articulo => articulo.id == e.target.id)
+            let posicionArticuloBuscado = carrito.findIndex(articulo => articulo.id == articuloBuscado.id)
 
-    if (posicionArticuloBuscado != -1) {
-        carrito[posicionArticuloBuscado].unidades++
-        carrito[posicionArticuloBuscado].subtotal = carrito[posicionArticuloBuscado].unidades * carrito[posicionArticuloBuscado].precioUnitario
+            if (posicionArticuloBuscado != -1) {
+                carrito[posicionArticuloBuscado].unidades++
+                carrito[posicionArticuloBuscado].subtotal = carrito[posicionArticuloBuscado].unidades * carrito[posicionArticuloBuscado].precioUnitario
 
-        carritoJSON = JSON.stringify(carrito)
-        localStorage.setItem("Carrito", carritoJSON)
-    } else {
-        carrito.push({
-            id: articuloBuscado.id, nombre: articuloBuscado.nombre, color: articuloBuscado.color, precioUnitario:
-                articuloBuscado.precio, unidades: 1, subtotal: articuloBuscado.precio
+                carritoJSON = JSON.stringify(carrito)
+                localStorage.setItem("Carrito", carritoJSON)
+            } else {
+                carrito.push({
+                    id: articuloBuscado.id, nombre: articuloBuscado.nombre, color: articuloBuscado.color, precioUnitario:
+                        articuloBuscado.precio, unidades: 1, subtotal: articuloBuscado.precio
 
-        })
-        carritoJSON = JSON.stringify(carrito)
-        localStorage.setItem("Carrito", carritoJSON)
-    }
-    localStorage.setItem("Carrito", JSON.stringify(carrito))
-    renderCarrito(carrito)
-}
-//funcion de renderizado carrito 
-function renderCarrito(arrayDeProductos) {
-    contenedorRenderCarrito.innerHTML = ''
-    for (const producto of arrayDeProductos) {
-        contenedorRenderCarrito.innerHTML += `
+                })
+                carritoJSON = JSON.stringify(carrito)
+                localStorage.setItem("Carrito", carritoJSON)
+                Toastify({
+                    text: "Producto agregado al carrito",
+                    duration: 2000,
+                    gravity: "bottom"
+                }).showToast();
+            }
+            localStorage.setItem("Carrito", JSON.stringify(carrito))
+            renderCarrito(carrito)
+        }
+
+        //funcion de renderizado carrito 
+        function renderCarrito(arrayDeProductos) {
+            contenedorRenderCarrito.innerHTML = ''
+            for (const producto of arrayDeProductos) {
+                contenedorRenderCarrito.innerHTML += `
         <div class="flex">
-        <table>
-        <theat>
-        <th>Nombre</th> <th>Color</th> <th>Precio Unitario</th><th>Unidades</th><th>Subtotal</th>
-        </theat>
-        <tr>
-        <td>${producto.nombre}</td> 
-        <td>${producto.color}</td>
-        <td>$${producto.precioUnitario}</td>
-        <td>${producto.unidades}</td>
-        <td>$${producto.subtotal}</td>
-        </tr>
-        </table>
+       <div>${producto.nombre}</div>
+       <div>${producto.color}</div>
+       <div>$${producto.precioUnitario}</div>
+       <div>${producto.unidades}</div>
+       <div>$${producto.subtotal}</div>
+        
         </div>
         `
 
-    }
-    let total = carrito.reduce((acc, valorActual) => acc + valorActual.subtotal, 0)
-    contenedorRenderCarrito.innerHTML += `
+            }
+            let total = carrito.reduce((acc, valorActual) => acc + valorActual.subtotal, 0)
+            contenedorRenderCarrito.innerHTML += `
         <div class="container-total"><h3>Total De Compra: ${total}$</h3></div>
         `
 
-}
+        }
 
-function ocultarSlider() {
-    document.getElementById("contenedor-slider").style.display = "none";
-    document.getElementById("contenedoRender").style.display = "flex";
-}
+        function ocultarSlider() {
+            document.getElementById("contenedor-slider").style.display = "none";
+            document.getElementById("contenedoRender").style.display = "flex";
+            Toastify({
+                text: "Boton Inicio para ir al Principio",
+                duration: 2500,
+                gravity: "bottom",
+                position: "top-right"
+            }).showToast();
+        }
 
-function volverInicio() {
-    document.getElementById("contenedor-slider").style.display = "block";
-    document.getElementById("contenedoRender").style.display = "none";
-}
-let vaciar = document.getElementById("vaciar")
-vaciar.addEventListener("click", vaciarCarrito)
+        function volverInicio() {
+            document.getElementById("contenedor-slider").style.display = "block";
+            document.getElementById("contenedoRender").style.display = "none";
+        }
+        let vaciar = document.getElementById("vaciar")
+        vaciar.addEventListener("click", vaciarCarrito)
 
-function vaciarCarrito() {
-    localStorage.removeItem("Carrito")
-    carrito = []
-    renderCarrito(carrito)
-}
+        function vaciarCarrito() {
+
+                Swal.fire({
+                    title: 'Borrado',
+                    text: 'Ha borrado los productos',
+                    imageUrl: '../images/logito.png',
+                    icon: 'warning',
+                    confirmButtonText: 'Borrar',
+                    imageAlt: 'imagen logo',
+                })
+            localStorage.removeItem("Carrito")
+            carrito = []
+            renderCarrito(carrito)
+        }
+    }
+
+
+
+
+
 
 
 
